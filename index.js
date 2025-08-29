6466105e7b17e74c2f5015b2fccb463126ae0e18
Aryan Manhas <2022A1R033@mietjammu.in>
2025-08-29 16:48:35 +0530

    input queries

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname , 'public')));
 app.set('view engine' , 'ejs');
 
+app.get("/", (req, res)=>
+{
+    fs.readdir(`./files` , (err , files)=>{
+        res.render("index" , {files : files}); //we use res.render when we need to render ejs pages ande index in views
+    })
+})
+
+app.post("/create", (req, res)=>
+{
+    fs.readdir(`./files` , (err , files)=>{
+        res.render("index" , {files : files}); //we use res.render when we need to render ejs pages ande index in views
+    })
+})
+
+app.listen(3000);
