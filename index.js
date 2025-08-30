a27b7419ec9ff6d405643e9e363e133bfe05df86
Aryan Manhas <2022A1R033@mietjammu.in>
2025-08-30 08:26:14 +0530

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
+const { log } = require('console');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -15,11 +16,16 @@ app.get("/", (req, res)=>
     })
 })
 
-app.post("/create", (req, res)=>
+app.post("/", (req, res)=>
 {
     fs.readdir(`./files` , (err , files)=>{
         res.render("index" , {files : files}); //we use res.render when we need to render ejs pages ande index in views
     })
+})
+app.post("/create", (req, res)=>
+{
+    console.log(req.body);
+    
 })
 
 app.listen(3000);
